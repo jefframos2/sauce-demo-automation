fc3b97d40a1c06e54d70f4386895a3491b3815b8
Jeff Ramos <ramosjeffersonm@gmai.com>
2026-02-28 13:59:44 +0800

    Added product item page object model

---
+++ components/ProductItem.js
@@ -0,0 +1,68 @@
+export class ProductItem {
+  constructor(page, itemName) {
+    this.page = page;
+    this.container = page
+      .locator('[data-test="inventory-item"]')
+      .filter({
+        has: page.locator('[data-test="inventory-item-name"]'),
+      })
+      .filter({
+        hasText: itemName,
+      });
+    this.nameLocator = this.container.locator(
+      '[data-test="inventory-item-name"]',
+    );
+    this.descripLocator = this.container.locator(
+      '[data-test="inventory-item-desc"]',
+    );
+    this.imageLocator = this.container.locator('img.inventory_item_img');
+    this.priceLocator = this.container.locator(
+      '[data-test="inventory-item-price"]',
+    );
+    this.addToCartBtnLocator = this.container.getByRole('button', {
+      name: 'Add to cart',
+      exact: true,
+    });
+    this.removeFromCartBtnLocator = this.container.getByRole('button', {
+      name: 'Remove',
+      exact: true,
+    });
+  }
+
+  async getName() {
+    return await this.nameLocator.innerText();
+  }
+
+  async getDescription() {
+    return await this.descripLocator.innerText();
+  }
+
+  async getImage() {
+    const baseUrl = 'https://www.saucedemo.com';
+    return `${baseUrl}${await this.imageLocator.getAttribute('src')}`;
+  }
+
+  async getPrice() {
+    return await this.priceLocator.innerText();
+  }
+
+  async view() {
+    await this.nameLocator.click();
+  }
+
+  async addToCart() {
+    if (!(await this.isAddedToCart())) {
+      await this.addToCartBtnLocator.click();
+    }
+  }
+
+  async removeFromCart() {
+    if (await this.isAddedToCart()) {
+      await this.removeFromCartBtnLocator.click();
+    }
+  }
+
+  async isAddedToCart() {
+    return await this.removeFromCartBtnLocator.isVisible();
+  }
+}
